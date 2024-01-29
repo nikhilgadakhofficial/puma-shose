dd8e6e27f652d55e370e1c4f914b27e959ab8bbc
nikhilgadakhofficial <nikhilgadakh543@gmail.com>
2024-01-29 21:46:04 +0530

    add page

---
+++ index.js
@@ -0,0 +1,370 @@
+
+      
+function likeOn1()
+{
+    const element = document.getElementById("on1");
+  element.src= "./img/bookmark (1).png";
+}
+
+        
+function cardCangeimg1()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "./img/shoes2.png";
+}
+         
+function cardCangeimg2()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "./img/shoes3.png";
+}
+         
+function cardCangeimg3()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "./img/shoes4.png";
+}
+  
+     
+function likeOn2()
+{
+    const element = document.getElementById("on2");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg4()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "./img/shoes2-2.png";
+}
+         
+function cardCangeimg5()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "./img/shoes2-3.png";
+}
+         
+function cardCangeimg6()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "./img/shoes2-4.png";
+}
+   
+   
+     
+function likeOn3()
+{
+    const element = document.getElementById("on3");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg7()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "./img/shoes3-1.png";
+}
+         
+function cardCangeimg8()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "./img/shoes3-3.png";
+}
+         
+function cardCangeimg9()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "./img/shoes3-4.png";
+}
+   
+    
+function likeOn4()
+{
+    const element = document.getElementById("on4");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg10()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "./img/shoes4-1.png";
+}
+         
+function cardCangeimg11()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "./img/shoes4-3.png";
+}
+         
+function cardCangeimg12()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "./img/shoes4-4.png";
+}
+   
+   
+   
+function likeOn5()
+{
+    const element = document.getElementById("on5");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg13()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "./img/shoes5-1.png";
+}
+         
+function cardCangeimg14()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "./img/shoes5-3.png";
+}
+         
+function cardCangeimg15()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "./img/shoes5-4.png";
+}
+   
+   
+ 
+function likeOn7()
+{
+    const element = document.getElementById("on7");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg19()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "./img/shoes7-2.png";
+}
+         
+function cardCangeimg20()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "./img/shoes7-3.png";
+}
+         
+function cardCangeimg21()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "./img/shoes7-4.png";
+}
+   
+  
+function likeOn8()
+{
+    const element = document.getElementById("on8");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg22()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "./img/shoes8-2.png";
+}
+         
+function cardCangeimg23()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "./img/shoes8-3.png";
+}
+         
+function cardCangeimg24()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "./img/shoes8-4.png";
+}
+   
+   
+function likeOn9()
+{
+    const element = document.getElementById("on9");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg25()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "./img/shoes9-2.png";
+}
+         
+function cardCangeimg26()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "./img/shoes9-3.png";
+}
+         
+function cardCangeimg27()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "./img/shoes9-4.png";
+}
+    
+function likeOn10()
+{
+    const element = document.getElementById("on10");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg28()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "./img/shoes10-2.png";
+}
+         
+function cardCangeimg29()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "./img/shoes10-3.png";
+}
+         
+function cardCangeimg30()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "./img/shoes10-4.png";
+}
+   
+ 
+function likeOn11()
+{
+    const element = document.getElementById("on11");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg31()
+{
+    const element = document.getElementById("js-img-11");
+  element.src= "./img/shoes11-2.png";
+}
+         
+function cardCangeimg32()
+{
+    const element = document.getElementById("js-img-11");
+  element.src= "./img/shoes11-3.png";
+}
+         
+function cardCangeimg33()
+{
+    const element = document.getElementById("js-img-11");
+  element.src= "./img/shoes11-4.png";
+}
+   
+ 
+function likeOn12()
+{
+    const element = document.getElementById("on12");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg34()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "./img/shoes12-2.png";
+}
+         
+function cardCangeimg35()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "./img/shoes12-3.png";
+}
+         
+function cardCangeimg36()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "./img/shoes12-4.png";
+}
+   
+
+function likeOn13()
+{
+    const element = document.getElementById("on13");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg37()
+{
+    const element = document.getElementById("js-img-13");
+  element.src= "./img/shoes13-2.png";
+}
+         
+function cardCangeimg38()
+{
+    const element = document.getElementById("js-img-13");
+  element.src= "./img/shoes13-3.png";
+}
+         
+function cardCangeimg39()
+{
+    const element = document.getElementById("js-img-13");
+  element.src= "./img/shoes13-4.png";
+}
+   
+
+function likeOn14()
+{
+    const element = document.getElementById("on14");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg40()
+{
+    const element = document.getElementById("js-img-14");
+  element.src= "./img/shoes14-2.png";
+}
+         
+function cardCangeimg41()
+{
+    const element = document.getElementById("js-img-14");
+  element.src= "./img/shoes14-3.png";
+}
+         
+function cardCangeimg42()
+{
+    const element = document.getElementById("js-img-14");
+  element.src= "./img/shoes14-4.png";
+}
+
+
+
+function likeOn15()
+{
+    const element = document.getElementById("on15");
+  element.src= "./img/bookmark (1).png";
+}
+
+       
+function cardCangeimg43()
+{
+    const element = document.getElementById("js-img-15");
+  element.src= "./img/shoes15-2.png";
+}
+         
+function cardCangeimg44()
+{
+    const element = document.getElementById("js-img-15");
+  element.src= "./img/shoes15-3.png";
+}
+         
+function cardCangeimg45()
+{
+    const element = document.getElementById("js-img-15");
+  element.src= "./img/shoes15-4.png";
+}
+   
+   
+  
